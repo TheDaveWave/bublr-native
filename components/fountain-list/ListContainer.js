6ddfee390f6dbae6af74959dae914b5f14c710c9
David Lindberg <david.wesley.lindberg@gmail.com>
2023-02-15 16:27:41 -0600

    started working on a listcontainer

--- components/fountain-list/ListContainer.js
+++ components/fountain-list/ListContainer.js
@@ -12,17 +12,14 @@ import Animated from "react-native-reanimated";
 
 const AnimatedView = Animated.createAnimatedComponent(View);
 
-function Ball() {
-  const translateX = useSharedValue(0);
+export default function ListContainer() {
   const translateY = useSharedValue(0);
 
   const onDrag = useAnimatedGestureHandler({
     onStart: (event, context) => {
-      context.translateX = translateX.value;
       context.translateY = translateY.value;
     },
     onActive: (event, context) => {
-      translateX.value = event.translationX + context.translateX;
       translateY.value = event.translationY + context.translateY;
     },
   });
@@ -31,9 +28,6 @@ function Ball() {
     return {
       transform: [
         {
-          translateX: translateX.value,
-        },
-        {
           translateY: translateY.value,
         },
       ],
@@ -41,18 +35,12 @@ function Ball() {
   });
 
   return (
-    <PanGestureHandler onGestureEvent={onDrag}>
-      <AnimatedView style={[containerStyle]}>
-        <View style={styles.ball} />
-      </AnimatedView>
-    </PanGestureHandler>
-  );
-}
-
-export default function TestList({ showModal, setShowModal }) {
-  return (
     <GestureHandlerRootView style={styles.container}>
-      <Ball />
+      <PanGestureHandler onGestureEvent={onDrag}>
+        <AnimatedView style={[containerStyle]}>
+          <View style={styles.slide} />
+        </AnimatedView>
+      </PanGestureHandler>
     </GestureHandlerRootView>
   );
 }
@@ -61,14 +49,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  content: {
-    backgroundColor: "#000",
-  },
-  ball: {
+  slide: {
     width: 100,
     height: 100,
-    borderRadius: 100,
     backgroundColor: "blue",
-    alignSelf: "center",
   },
 });
--- screens/FountainList.js
+++ screens/FountainList.js
@@ -1,35 +1,21 @@
-import { useState } from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import ListModal from "../components/fountain-list/ListModal";
-import TestList from "../components/fountain-list/TestList";
+import ListContainer from "../components/fountain-list/ListContainer";
 
 import { testData } from "../test-data/fountain-data";
 
-const picture = require("../assets/fountain-images/eda-fountain1.jpeg");
-
-
 export default function FountainList({ navigation }) {
-  const [showModal, setShowModal] = useState(true);
-
   return (
-    // <SafeAreaView 
-    //   // style={styles.container}
-    // >
-      /* <Text>This will be a list of founatins.</Text>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={picture} resizeMode="contain" />
-      </View> */
-      /* <ListModal showModal={showModal} setShowModal={setShowModal}/> */
-      <TestList />
-    /* </SafeAreaView> */
+    <SafeAreaView style={styles.container}>
+      <ListContainer />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000",
+    // backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
   },
