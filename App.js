3b8fdb16d650e76aab84e6d1e4a9ecaedb5374aa
David Lindberg <david.wesley.lindberg@gmail.com>
2023-02-21 12:59:48 -0600

    successfully uploaded image to firebase storage

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import 'react-native-gesture-handler';
+import { initializeApp } from "firebase/app";
+import "react-native-gesture-handler";
+import firebaseConfig from "./firebaseConfig.js";
 // Providers:
 
 // Navigation imports:
 import AppNav from "./navigators/AppNav";
 
+initializeApp(firebaseConfig);
+
 export default function App() {
-  
-  return (
-    <AppNav />
-  );
+  return <AppNav />;
 }
--- screens/settings-screens/AddFountain.js
+++ screens/settings-screens/AddFountain.js
@@ -4,8 +4,8 @@ import { Camera, CameraType } from "expo-camera";
 import * as MediaLibrary from "expo-media-library";
 import * as Location from "expo-location";
 import * as ImagePicker from "expo-image-picker";
-// import { ref, onValue, push, update, remove } from "firebase/database";
 import { getStorage, ref, uploadBytes } from "firebase/storage";
+
 // Component Imports:
 import CircleButton from "../../components/buttons/camera-buttons/CircleButton";
 import CameraButton from "../../components/buttons/camera-buttons/CameraButton";
@@ -21,8 +21,6 @@ export default function AddFountain({ navigation }) {
   const [permission, requestPermission] = MediaLibrary.usePermissions();
   const [locationPermission, requestLocationPermission] =
     Location.useForegroundPermissions();
-
-  const storage = getStorage();
 
   // update these if statements.
   if (!permission) {
@@ -54,18 +52,18 @@ export default function AddFountain({ navigation }) {
     if (ready) {
       setPaused(true);
       await cameraRef
-        .takePictureAsync()
+        .takePictureAsync({ base64: true, quality: 1 })
         .then((response) => {
           console.log("picture taken:", response);
-          setPicture(response.uri);
+          setPicture(response);
         })
         .catch((err) => {
           console.log("Error taking picture:", err);
         });
-     /*  let result = await ImagePicker.launchCameraAsync({ base64: true });
+      /* let result = await ImagePicker.launchCameraAsync({ base64: true });
       result = result.assets[0];
-      console.log(result);
-      setPicture(result.uri); */
+      console.log(result); */
+      // setPicture(result.uri);
     } else {
       alert("Lol you can't take the picture!");
     }
@@ -87,15 +85,15 @@ export default function AddFountain({ navigation }) {
         console.log("Error getting location", err);
       }); */
     console.log("Location:", location);
-    console.log("URI:", picture);
-    // change file size?
 
     // create blob
-    const blob = new Blob([picture]);
-    console.log("blob:", blob, `blob size: ${blob.size} bytes`);
+    /* const blob = new Blob([picture.uri]);
+    console.log("blob:", blob, `blob size: ${blob.size} bytes`); */
     // upload photo to firebase.
-      // const fountainRef = ref(storage)
+    await uploadImage();
     // get url and add to database on firebase.
+
+    // Download blob to device
 
     /* await MediaLibrary.saveToLibraryAsync(picture);
     alert("Picture saved."); */
@@ -103,6 +101,26 @@ export default function AddFountain({ navigation }) {
     setPaused(false);
 
     // navigation.goBack();
+  }
+
+  async function uploadImage() {
+    const storage = getStorage();
+    const storageRef = ref(storage, "images-test2");
+
+    const img = await fetch(picture.uri);
+    const file = await img.blob();
+
+    uploadBytes(storageRef, file).then((result) => {
+      console.log(result);
+    });
+
+    // await uploadBytes(storageRef, file)
+    //   .then((response) => {
+    //     console.log(response);
+    //   })
+    //   .catch((err) => {
+    //     console.log("error uploading to firebase", err);
+    //   });
   }
 
   return (
@@ -115,7 +133,7 @@ export default function AddFountain({ navigation }) {
           type={CameraType.back}
         ></Camera>
       ) : (
-        <ImageBackground style={styles.camera} source={{ uri: picture }} />
+        <ImageBackground style={styles.camera} source={{ uri: picture.uri }} />
       )}
       <View style={styles.buttonContainer}>
         <CameraButton icon="done" onPress={() => submit()} disabled={!paused} />
