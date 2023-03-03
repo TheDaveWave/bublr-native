11eef13f45852bc6a428596dd77bb3e0aa68c948
David Lindberg <david.wesley.lindberg@gmail.com>
2023-03-03 11:19:19 -0600

    started styling the login screen, created a folder called styles to later refactor all styling for components into separate files

--- screens/auth-screens/Login.js
+++ screens/auth-screens/Login.js
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import { Text, View, TextInput, Button } from "react-native";
 import { AuthContext } from "../../context/AuthContext";
+import styles from "../../styles/styles";
 
 export default function LoginScreen({ navigation }) {
   const { emailSignIn, emailSignUp } = useContext(AuthContext);
@@ -9,27 +10,30 @@ export default function LoginScreen({ navigation }) {
   const [password, setPassword] = useState("");
 
   function submit() {
-    emailSignUp(email, password);
+    emailSignIn(email, password);
     setEmail("");
     setPassword("");
   }
 
   return (
-    <View>
-      <Text>This is the login screen.</Text>
-      <TextInput
-        onChangeText={setEmail}
-        value={email}
-        textContentType="emailAddress" // iOS only
-        placeholder="Email"
-      />
-      <TextInput
-        onChangeText={setPassword}
-        value={password}
-        secureTextEntry={true}
-        placeholder="Password"
-      />
-      <Button title="Submit" onPress={() => submit()} />
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={setEmail}
+          value={email}
+          textContentType="emailAddress" // iOS only
+          placeholder="Email"
+          style={styles.input}
+        />
+        <TextInput
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry={true}
+          placeholder="Password"
+          style={styles.input}
+        />
+        <Button title="Submit" onPress={() => submit()} />
+      </View>
     </View>
   );
 }
---
+++ styles/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  input: {
+    margin: 10,
+    padding: 5,
+    borderColor: "#000",
+    borderWidth: 1,
+    backgroundColor: "#fff",
+    width: "70%",
+  },
+});
+
+export default styles;
